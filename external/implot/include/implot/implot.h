b7b945eb69eaac112a13b7e3294d0efdec3cf826
Pebecko <haderad@gmail.com>
2026-03-25 22:10:45 +0200

    Added additional files to ImPlot submodule

---
+++ external/implot/include/implot/implot.h
@@ -0,0 +1,6 @@
+#pragma once
+//#include <imgui.h>
+//#define IMGUI_DEFINE_MATH_OPERATORS
+//#define ImGuiKeyModFlags ImGuiKey;
+//typedef ImGuiModFlags ImGuiKeyModFlags
+#include <../../implot/vendor/implot.h>
