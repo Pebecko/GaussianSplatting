11bda09e08e2c94d4c764c3bac60284f9c2fbe20
Pebecko <haderad@gmail.com>
2026-03-25 22:08:18 +0200

    Added additional files to stb submodule

---
+++ external/stb/stbImplementations.cpp
@@ -0,0 +1,15 @@
+/*
+ * defines for stb library
+ */
+
+// stb_image
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+// stb_image_write
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb/stb_image_write.h>
+
+// stb_image_resize2
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include <stb/stb_image_resize2.h>
